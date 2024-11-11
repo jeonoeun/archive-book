fbb8913348efdb97ac212da6154915fc4d730e65
Jeongeun Lee <h00kk@naver.com>
2024-11-11 19:53:56 +0900

    ✨Feat: SocialLoginButton 추가

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,11 +4,16 @@ import SocialoginButton from "@/components/SocialoginButton";
 
 export default function Login() {
   return (
-    <>
-      <Image src={LogoImg} alt="logo" />
-      <p>로그인 후 이용가능합니다.</p>
-
-      <SocialoginButton />
-    </>
+    <div className="p-5">
+      <div className="flex flex-col items-center mt-14">
+        <div className="mb-6">
+          <Image src={LogoImg} alt="logo" />
+        </div>
+        <p className="mb-20 text-[#9CABBB]">로그인 후 이용가능합니다.</p>
+        <SocialoginButton label="kakao" />
+        <SocialoginButton label="naver" />
+        <SocialoginButton label="google" />
+      </div>
+    </div>
   );
 }
--- src/components/SocialoginButton.tsx
+++
@@ -1,3 +0,0 @@
-export default function SocialoginButton() {
-  return <button>SocialoginButton</button>;
-}
