885c053ccba0bb0df495461fd074cbb9910ad102
Jeongeun Lee <zaekoch@gmail.com>
2024-11-30 16:42:00 +0900

    design: Style BookDetailInfo component

--- src/components/commons/header/CustomHeader.tsx
+++ src/components/commons/header/CustomHeader.tsx
@@ -6,9 +6,10 @@ import { GrPrevious } from "react-icons/gr";
 export default function CustomHeader() {
   const router = useRouter();
   return (
-    <header className="p-5 fixed top-0 left-0 w-full z-10">
-      <button onClick={() => router.back()}>
+    <header className="p-5 fixed top-0 left-0 w-full z-10 flex items-center justify-between">
+      <button onClick={() => router.back()} className="flex items-center gap-2">
         <GrPrevious />
+        <span>뒤로</span>
       </button>
     </header>
   );
--- src/components/domains/book/BookDetailInfo.tsx
+++ src/components/domains/book/BookDetailInfo.tsx
@@ -2,6 +2,17 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { MdKeyboardArrowRight, MdKeyboardArrowDown } from "react-icons/md";
+
+const InfoListItem = ({ title, des, children }) => {
+  return (
+    <div className="flex gap-[6px]">
+      <span className="text-[#9CABBB]">{title}</span>
+      {des && <p>{des}</p>}
+      {children}
+    </div>
+  );
+};
 
 export default function BookDetailInfo({ book }) {
   return (
@@ -22,52 +33,82 @@ export default function BookDetailInfo({ book }) {
             src={book.thumbnail}
             alt=""
             layout="fill"
-            objectFit="cover"
+            style={{ objectFit: "cover" }}
             priority
           />
         </div>
       </div>
-      <div className="flex flex-col gap-3 px-5 py-7">
-        <p className="font-semibold text-2xl mb-1">{book.title}</p>
-        <div className="flex gap-[6px]">
-          <span className="text-[#9CABBB]">저자</span>
-          <div className="flex gap-1">
-            {book.authors.map((author) => (
-              <span key={author}>{author}</span>
-            ))}
-          </div>
+      {/* bookInfo */}
+      <div>
+        <div className="flex flex-col gap-3 px-5 py-6">
+          <p className="font-semibold text-2xl">{book.title}</p>
+          <InfoListItem title="저자">
+            <div className="flex gap-1">
+              {book.authors.map((author, index) => (
+                <span key={`${author}-${index}`}>{author}</span>
+              ))}
+            </div>
+          </InfoListItem>
+          <InfoListItem title="출판사" des={book.publisher} />
+          <InfoListItem title="출판일" des={book.datetime?.slice(0, 10)} />
+          <InfoListItem title="설명" des={book.contents} />
+          <InfoListItem title="가격" des={book.price} />
+          <InfoListItem title="상태" des={book.status} />
+          <Link href={book.url} target="_blank" rel="noopener noreferrer">
+            다음에서 자세히 보기
+          </Link>
         </div>
-        <div className="flex gap-[6px]">
-          <span className="text-[#9CABBB]">출판사</span>
-          <p>{book.publisher}</p>
-        </div>
-        <div className="flex gap-[6px]">
-          <span className="text-[#9CABBB]">출판일</span>
-          <p>{book.datetime?.slice(0, 10)}</p>
-        </div>
-        <div className="flex gap-[6px]">
-          <span className="text-[#9CABBB]">설명</span>
-          <p>{book.contents}</p>
-        </div>
-        <div className="flex gap-[6px]">
-          <span className="text-[#9CABBB]">가격</span>
-          <p>{book.price}</p>
-        </div>
-        <div className="flex gap-[6px]">
-          <span className="text-[#9CABBB]">상태</span>
-          <p>{book.status}</p>
-        </div>
-        <Link href={book.url} target="_blank">
-          다음에서 자세히 보기
-        </Link>
         <button
           onClick={() => console.log(book)}
-          className="border border-[#ECEEF0] py-[13px] rounded mt-4"
+          className="flex items-center justify-center gap-1 border-t border-[#f1f1f1] py-3 w-full"
         >
-          더보기
+          <span>더보기</span>
+          <MdKeyboardArrowDown />
         </button>
       </div>
-      <div className="bg-[#F8F9FD] h-[14px]"></div>
+
+      <div className="bg-[#F8F9FD] h-[14px] border-t border-b border-[#f1f1f1]" />
+      {/* 나의 리뷰 */}
+      <div>
+        <div className="flex justify-between border-b border-[#F5F7FA] p-5">
+          <div className="font-semibold">⭐️ ⭐️ ⭐️ ⭐️ ⭐️ 5.0</div>
+          <div className="flex items-center justify-center gap-1">
+            <span>나의 리뷰</span>
+            <MdKeyboardArrowRight />
+          </div>
+        </div>
+        <div className="flex justify-between border-b border-[#F5F7FA] p-5">
+          <div className="font-semibold">나의 문장 수집</div>
+          <div className="flex items-center justify-center gap-1">
+            <span>5개</span>
+            <MdKeyboardArrowRight />
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-[#F8F9FD] h-[14px] border-t border-b border-[#f1f1f1]" />
+
+      {/* 다른 사람 리뷰 */}
+      <div>
+        <div className="flex justify-between border-b border-[#F5F7FA] p-5">
+          <div className="flex items-center gap-2 font-semibold">
+            <p>이 책이 담긴 리뷰</p>
+            <span className="text-[#FCC33C]">15</span>
+          </div>
+          <button>
+            <MdKeyboardArrowRight />
+          </button>
+        </div>
+        <div className="flex justify-between border-b border-[#F5F7FA] p-5">
+          <div className="flex items-center gap-2 font-semibold">
+            <p>이 책이 담긴 문장 수집</p>
+            <span className="text-[#FCC33C]">127</span>
+          </div>
+          <button>
+            <MdKeyboardArrowRight />
+          </button>
+        </div>
+      </div>
     </>
   );
 }
